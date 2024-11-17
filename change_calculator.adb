aab5300ac4c0ff380194127d1c61e152555f309d
junsevith <pawelstanik44@gmail.com>
2024-11-17 01:01:06 +0100

    sgd

---
+++ change_calculator.adb
@@ -0,0 +1,81 @@
+with Ada.Text_IO;
+with Ada.Float_Text_IO;
+with Ada.IO_Exceptions;
+with Ada.Integer_Text_IO;
+use Ada;
+
+procedure Change_Calculator is
+   type Money is delta 0.01 digits 10;
+   Input_Amount : Money := 0.0;
+   package Money_IO is new Text_IO.Decimal_IO(Money);
+
+   type Currency_Denomination is record
+      Name : String(1..10);
+      Value : Money;
+   end record;
+
+
+   Currency_Names  : array (1..10) of Currency_Denomination;
+   Currency_Counts : array (1..10) of Integer := (others=>0);
+   Currency_Index  : Integer range 1..10;
+
+   procedure Get_Money_Prompt(Amount: out Money) is
+      Response : String(1..20);
+      Last     : Natural;
+   begin
+      loop
+         declare
+         begin
+            Text_IO.Put("amount: ");
+            Text_IO.Flush;
+            Text_IO.Get_Line(Response, Last);
+            -- try to convert string input to money type
+            Amount := Money'Value(Response(1 .. Last));
+            -- quit the loop if the money converted
+            exit;
+         exception
+            when Constraint_Error =>
+               Text_IO.Put_Line("ERROR: bad money format");
+         end;
+      end loop;
+   end Get_Money_Prompt;
+
+begin
+   Currency_Names := (("Penny     ", 0.01),
+                      ("Nickle    ", 0.05),
+                      ("Dime      ", 0.10),
+                      ("Quarter   ", 0.25),
+                      ("Dollar    ", 1.00),
+                      ("5 Dollar  ", 5.00),
+                      ("10 Dollar ", 10.00),
+                      ("20 Dollar ", 20.00),
+                      ("50 Dollar ", 50.00),
+                      ("100 Dollar", 100.00));
+
+   loop
+      -- read money amount from the user
+      Get_Money_Prompt(Input_Amount);
+
+      -- calculate needed currency
+      for I in Currency_Names'Range loop
+         -- calculate inverse of index to count backwords
+         Currency_Index := (Currency_Names'Length-I)+1;
+         while Input_Amount >= Currency_Names(Currency_Index).Value loop
+            Input_Amount := Input_Amount - Currency_Names(Currency_Index).Value;
+            -- increment currency useage in array
+            Currency_Counts(I) := Currency_Counts(I) + 1;
+         end loop;
+      end loop;
+
+      -- display needed currency
+      for I in Currency_Counts'Range loop
+         Currency_Index := (Currency_Names'Length-I)+1;
+         -- do not display unused currency
+         if Currency_Counts(Currency_Index) /= 0 then
+            Text_IO.Put(Currency_Names(I).Name & " ");
+            Integer_Text_IO.Put(Currency_Counts(Currency_Index));
+            Text_IO.New_Line;
+         end if;
+      end loop;
+   end loop;
+end Change_Calculator;
--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 7
-#define YY_END_OF_BUFFER 8
+#define YY_NUM_RULES 2
+#define YY_END_OF_BUFFER 3
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,19 +360,18 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[15] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    8,    6,    5,    1,    7,    3,    6,    5,
-        4,    2,    3,    0
+        0,    0,    3,    2,    2,    0,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
-        4,    4,    4,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    3,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -398,33 +397,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[5] =
+static const YY_CHAR yy_meta[4] =
     {   0,
-        1,    2,    3,    2
+        1,    1,    2
     } ;
 
-static const flex_int16_t yy_base[17] =
+static const flex_int16_t yy_base[13] =
     {   0,
-        0,    9,   10,    0,    3,    0,   12,    7,    0,    0,
-       12,    0,    0,   12,    7,    4
+        7,    6,    8,   11,    0,    5,    4,   11,   11,    0,
+        3,    2
     } ;
 
-static const flex_int16_t yy_def[17] =
+static const flex_int16_t yy_def[13] =
     {   0,
-       14,    1,   14,   15,   14,   16,   14,    5,   15,    5,
-       14,   16,    8,    0,   14,   14
+       10,   10,    9,    9,   11,   12,   12,    9,    0,    9,
+        9,    9
     } ;
 
-static const flex_int16_t yy_nxt[17] =
+static const flex_int16_t yy_nxt[15] =
     {   0,
-        4,    5,    6,    7,   10,   11,   12,    9,   13,   14,
-        8,    3,   14,   14,   14,   14
+        4,    4,    7,    7,    6,    8,    8,    9,    5,    5,
+        3,    9,    9,    9
     } ;
 
-static const flex_int16_t yy_chk[17] =
+static const flex_int16_t yy_chk[15] =
     {   0,
-        1,    1,    1,    1,    5,    5,   16,   15,    8,    3,
-        2,   14,   14,   14,   14,   14
+       10,   10,   12,   12,   11,    7,    6,    3,    2,    1,
+        9,    9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -441,15 +440,15 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "zad1.l"
-#line 2 "zad1.l"
+#line 1 "zad2.l"
+#line 2 "zad2.l"
 #include <stdio.h>
 #include <string.h>
 
 int line_count = 0;  // Liczba linii
 int word_count = 0;  // Liczba słów
+#line 451 "lex.yy.c"
 #line 452 "lex.yy.c"
-#line 453 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -629,9 +628,6 @@ extern int yylex (void);
 #endif
 
 #define YY_RULE_SETUP \
-	if ( yyleng > 0 ) \
-		YY_CURRENT_BUFFER_LVALUE->yy_at_bol = \
-				(yytext[yyleng - 1] == '\n'); \
 	YY_USER_ACTION
 
 /** The main scanner function which does all the work.
@@ -669,10 +665,10 @@ YY_DECL
 		}
 
 	{
-#line 9 "zad1.l"
+#line 9 "zad2.l"
 
 
-#line 676 "lex.yy.c"
+#line 672 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -687,7 +683,6 @@ YY_DECL
 		yy_bp = yy_cp;
 
 		yy_current_state = (yy_start);
-		yy_current_state += YY_AT_BOL();
 yy_match:
 		do
 			{
@@ -700,13 +695,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 15 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 12 );
+		while ( yy_base[yy_current_state] != 11 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -731,54 +726,19 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
-/* rule 1 can match eol */
-YY_RULE_SETUP
-#line 11 "zad1.l"
-{
-    line_count++;
-    printf("\n");
-}
-	YY_BREAK
-case 2:
-/* rule 2 can match eol */
-YY_RULE_SETUP
-#line 16 "zad1.l"
-{printf("\n");}
-	YY_BREAK
-case 3:
-YY_RULE_SETUP
-#line 18 "zad1.l"
-{}
-	YY_BREAK
-case 4:
 *yy_cp = (yy_hold_char); /* undo effects of setting up yytext */
 (yy_c_buf_p) = yy_cp -= 1;
 YY_DO_BEFORE_ACTION; /* set up yytext again */
 YY_RULE_SETUP
-#line 20 "zad1.l"
+#line 11 "zad2.l"
 {}
 	YY_BREAK
-case 5:
+case 2:
 YY_RULE_SETUP
-#line 22 "zad1.l"
-{ 
-    printf(" ");
-}
-	YY_BREAK
-case 6:
-YY_RULE_SETUP
-#line 26 "zad1.l"
-{ 
-    word_count++;
-    printf("%s", yytext);
-}
-	YY_BREAK
-case 7:
-YY_RULE_SETUP
-#line 32 "zad1.l"
+#line 13 "zad2.l"
 ECHO;
 	YY_BREAK
-#line 782 "lex.yy.c"
+#line 742 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1063,7 +1023,6 @@ static int yy_get_next_buffer (void)
 	char *yy_cp;
     
 	yy_current_state = (yy_start);
-	yy_current_state += YY_AT_BOL();
 
 	for ( yy_cp = (yytext_ptr) + YY_MORE_ADJ; yy_cp < (yy_c_buf_p); ++yy_cp )
 		{
@@ -1076,7 +1035,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 15 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1104,11 +1063,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 15 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 14);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1223,8 +1182,6 @@ static int yy_get_next_buffer (void)
 	c = *(unsigned char *) (yy_c_buf_p);	/* cast for 8-bit char's */
 	*(yy_c_buf_p) = '\0';	/* preserve yytext */
 	(yy_hold_char) = *++(yy_c_buf_p);
-
-	YY_CURRENT_BUFFER_LVALUE->yy_at_bol = (c == '\n');
 
 	return c;
 }
@@ -1786,13 +1743,15 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 32 "zad1.l"
+#line 13 "zad2.l"
 
 
 int yywrap() {}
 
 int main(int argc, char **argv) {
-    if (argc > 1) {
+    if (argc == 0) {
+        printf("Please provide file as argument");
+    } else if (argc > 1) {
         FILE *f = fopen(argv[1], "r");
         if (f == NULL) {
             perror("Error opening file");
@@ -1801,10 +1760,7 @@ int main(int argc, char **argv) {
         yyin = f;
     }
 
-    yylex();  // Rozpoczynamy analizę leksykalną
-
-    // Dopisujemy liczbę linii i słów
-    printf("\nLines: %d, Words: %d\n", line_count, word_count);
+    yylex();
 
     return 0;
 
